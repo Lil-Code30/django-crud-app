0c2bb158d7015d7746af2c258ca0113200efa9b7
Ismael Loko <lokoismael30@gmail.com>
2024-11-05 10:35:35 -0500

    Adding the logic to add employee infos in the db

--- gestion/urls.py
+++ gestion/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add-employee', views.add_employee, name='add_employee'),
+    path('add-employee/', views.add_employee, name='add_employee'),
+    path('register/', views.add_records, name='add_records'),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import  messages
 from .models import Employee
 
 # Create your views here.
@@ -12,3 +13,21 @@ def index(request):
 def add_employee(request):
     return  render(request, 'add_employee.html')
 
+def add_records(request):
+    if request.method  == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        telephone = request.POST['telephone']
+        postal_code = request.POST['postalcode']
+        position = request.POST['position']
+
+        new_employee = Employee(firstname=firstname, lastname=lastname, email=email, telephone=telephone, postal_code=postal_code, position=position)
+        new_employee.save()
+
+        if new_employee.save:
+            messages.success(request, 'New Employee added successfully')
+            return redirect('index')
+        else:
+            messages.error(request, 'There was an error, Please Try Again')
+            return redirect('add_employee')
