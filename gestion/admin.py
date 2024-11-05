845d82e8df0dadf87ce0eaa69506b5d65f0654e0
Ismael Loko <lokoismael30@gmail.com>
2024-11-05 00:06:34 -0500

    Registering the Employee model to the admin page

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'email', 'telephone', 'postal_code', 'position', 'created_at')
+
+admin.site.register(Employee,  EmployeeAdmin)
