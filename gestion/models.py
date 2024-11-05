d33d6a3a4934068a08aaac76d756d4b12ecb8e38
Ismael Loko <lokoismael30@gmail.com>
2024-11-04 23:39:22 -0500

    Creating employee model and applying migrations

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=40)
+    postal_code = models.CharField(max_length=6)
+    position = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
